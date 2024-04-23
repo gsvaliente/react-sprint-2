7d16f479c7b5140c528b94243032b3cfc61c1774
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-04-23 10:51:10 +0200

    Completed ALL exercises

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This repo is for Sprint 2 of React - JavaScript 1
 
-## ARROW FUNCITONS
+## ARROW FUNCTIONS
 
 ### Level 1 Exercises
 
@@ -30,6 +30,92 @@ This repo is for Sprint 2 of React - JavaScript 1
 1. Link ternary operator: write an expression that uses links of ternary operators to detemrine whether a number is positive, negative, or zero
 2. Create a findMaximum function that accepts three parameters (a,b,c) use the ternary operator to determine the max value
 
-### Level 3 Excercises
+### Level 3 Exercises
 
 1. Create oddOrEven function that accepts an array of numbers and uses a for loop to iterate through the array. Inside the loop use the ternary operator to determine if each number is odd or even
+
+## CALLBACKS
+
+### Level 1 Exercises
+
+1. Write a callback function called process that accepts two parameters, (number, cb). The function must call the cb function, passing the number as parameter
+2. Write a calculator function that accepts three parameters, (num1,num2,cb). The function must call the cb with the two numbers as parameter.
+
+### Level 2 Exercises
+
+1. Async function: write waitForHello function that accepts (name,cb). The function must then wait 2 seconds and then invoke the cb. Passing the name as parameter
+2. With Arrays: write processElements function that accepts (array,cb). The process must call the cb for each element of the array
+
+### Level 3 Exercises
+
+1. Write processString, it accepts (string, cb). The function must convert the string to uppercase and then call the cb with the transformed string
+
+## REST & SPREAD
+
+### Level 1 Exercises
+
+1. Create two arrays, use the spread operator to create a third array containing all the elements of both created arrays
+
+2. Create a 'sum' function that uses the rest operator to accept an undetermined number of arguments and return their sum
+
+### Level 2 Exercises
+
+1. Create an object, then create another one which is a copy of the first. Change a property of the second without changing it in the first
+2. Create an array with several items, asign the first 2 to variables, and the rest to another
+
+### Level 3 Exercises
+
+1. Create a function that accepts three parameters and call the function using the spread operator with that array
+
+2. Create two objects with different properties, ue the spread operator to merge them
+
+## ARRAY TRANSFORMATIONS
+
+### Level 1 Exercises
+
+1. Map: Create a new array containing the square of each number
+2. Filter: Create a new array containing the even numbers
+3. Find: Use the find method to find the first number that is greater than 10
+4. Reduce: Calculate the total sum of the numbers
+
+### Level 2 Exercises
+
+1. Create a one-line function that filters > 10, multiplies by 2, calculates the sum, returns value
+
+### Level 3 Exercises
+
+1. Every/Some: use both to determine if all or some of the elements in the array are > 10
+
+## ARRAY LOOP
+
+### Level 1 Exercises
+
+1. forEach: have an array of names, use forEach to print each name
+2. forOff: use it to print each name to the console
+3. use filter to create a new array that contains only the even numbers
+
+### Level 2 Exercises
+
+1. forIn: use a forIn loop to print each key and its value to console
+2. forOf with break: print the numbers of an array but stop at 5
+
+### Level 3 Exercises
+
+1. forOf with index: use a forLoop to print the console element of the array and its index
+
+## PROMISES & ASYNC/AWAIT
+
+### Level 1 Exercises
+
+1. create a promise that resolves after 2 seconds ands returns 'Hello, world'
+2. Use the previous promise, create a .then that prints the result to console
+3. Create a promise that resolves after 2 seconds if the text is 'Hello, world', if not, reject it
+4. Use async/await to wait for the promise created in 1.
+
+### Level 2 Exercises
+
+1. Error Handle: Modify the function 4. Use a try/catch block to look for possible errors
+
+### Level 3 Exercises
+
+1. Promise.all: create 2 promises that resolve after 2, 3 seconds. Use promise.all to wait for both promises to resolve and print to console
--- index.js
+++ index.js
@@ -64,3 +64,240 @@ function oddOrEven(list) {
     console.log(info);
   }
 }
+
+// CALLBACKS
+// Exercise 3.1
+const process = (num, cb) => {
+  cb(num);
+};
+
+const printNumber = (num) => {
+  console.log(num);
+};
+
+process(9, printNumber);
+
+// Exercise 3.2
+const calculator = (num1, num2, cb) => {
+  console.log(cb(num1, num2));
+};
+
+calculator(3, 8, add);
+
+// Exercise 3.3
+const waitForHello = (name, cb) => {
+  setTimeout(() => {
+    cb(name);
+  }, 2000);
+};
+
+const printName = (name) => {
+  console.log(`Hello, my name is ${name}`);
+};
+
+waitForHello("Gabriel", printName);
+
+// Exercise 3.4
+const animals = ["cat", "dog", "fish", "bird", "rabbit"];
+const processElements = (arr, cb) => {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i]);
+  }
+};
+
+const printElement = (el) => {
+  console.log(el);
+};
+
+processElements(animals, printElement);
+
+// Exercise 3.5
+const randomWord = "helloworld";
+
+const processString = (str, cb) => {
+  const newWord = str.toUpperCase();
+  cb(newWord);
+};
+
+processString(randomWord, printElement);
+
+// SPREAD AND REST OPERATORS
+// Exercies 4.1
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [6, 7, 8, 9, 10];
+
+const combinedArrays = [...arr1, ...arr2];
+console.log(combinedArrays);
+// Exercise 4.2
+const sum = (...args) => {
+  return args.reduce((acc, curr) => acc + curr, 0);
+};
+console.log(sum(1, 2, 3, 4, 5, 6, 7));
+
+// Exercise 4.3
+const user = {
+  name: "Gabriel",
+  age: 25,
+  country: "El Salvador",
+};
+
+const user2 = { ...user };
+user2.age = 31;
+console.log({ user, user2 });
+
+// Exercise 4.4
+const randomArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const [first, second, ...rest] = randomArray;
+
+// Exercise 4.5
+const colors = ["red", "blue", "green"];
+
+const printColors = (color1, color2, color3) => {
+  console.log(color1, color2, color3);
+};
+printColors(...colors);
+
+// Exercise 4.6
+const car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2015,
+};
+const looks = {
+  color: "black",
+  rims: "silver",
+};
+
+const newCar = { ...car, ...looks };
+console.log(newCar);
+
+// ARRAY TRANSFORMATION
+// Exercise 5.1
+const mapArray = [1, 2, 3, 4];
+const mappedArray = mapArray.map((num) => num * 2);
+
+// Exercise 5.2
+const filterArray = [1, 2, 3, 4];
+const filteredArray = filterArray.filter((num) => num % 2 === 0);
+
+// Exercise 5.3
+const findArray = [1, 10, 8, 11];
+const foundArray = findArray.find((num) => num > 10);
+
+// Exercise 5.4
+const reduceArray = [13, 7, 8, 21];
+const reducedArray = reduceArray.reduce((acc, curr) => acc + curr, 0);
+
+// Exercise 5.5
+const oneLineArray = [1, 3, 7, 10, 15, 17, 11, 5, 8, 12, 9];
+
+const megaArray = () =>
+  oneLineArray
+    .filter((num) => num >= 10)
+    .map((num) => num * 2)
+    .reduce((acc, curr) => acc + curr, 0);
+console.log(megaArray());
+
+// Exercise 5.6
+const everySomeArray = [11, 12, 13, 14];
+
+const everyArray = everySomeArray.every((num) => num > 10);
+
+const someArray = everySomeArray.some((num) => num > 10);
+
+// ARRAY LOOPS
+// Exercise 6.1
+const names = ["Anna", "Bernat", "Clara"];
+
+names.forEach((name) => console.log(name));
+
+// Exercise 6.2
+for (const name of names) {
+  console.log(name);
+}
+
+// Exercise 6.3
+const filterArray2 = [1, 2, 3, 4, 5, 6];
+
+const filteredArray2 = filterArray2.filter((num) => num % 2 === 0);
+console.log(filteredArray2);
+
+// Exercise 6.4
+const forInObject = {
+  name: "Gabriel",
+  age: 31,
+  country: "El Salvador",
+};
+
+for (const key in forInObject) {
+  console.log(key, forInObject[key]);
+}
+
+// Exercise 6.5
+for (const num of filterArray2) {
+  if (num > 5) break;
+  console.log(num);
+}
+
+// Exercise 6.6
+for (const [idx, name] of names.entries()) {
+  console.log(name, idx);
+}
+
+// PROMISES AND ASYNC/AWAIT
+// Exercise 7.1
+const firstPromise = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("Hello, world");
+  }, 2000);
+});
+
+// Exercise 7.2
+firstPromise.then((res) => console.log(res));
+
+// Exercise 7.3
+const getValidation = (input) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (input === "Hello") {
+        resolve("Success");
+      }
+      reject("Error");
+    }, 2000);
+  });
+};
+getValidation("Hello")
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+// Exercise 7.4
+const asyncFunction = async () => {
+  const res = await firstPromise;
+  console.log(res);
+};
+asyncFunction();
+
+// Exercise 7.5
+const asyncFunction2 = async () => {
+  try {
+    const res = await firstPromise;
+    console.log(res);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Exercise 7.6
+const promiseWait1 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("first promise");
+  }, 2000);
+});
+
+const promiseWait2 = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("second promise");
+  }, 3000);
+});
+
+Promise.all([promiseWait1, promiseWait2]).then((res) => console.log(res));
